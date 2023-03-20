5fbf62c3a91dbd865b85d0b75ab1896bcc3f1e58
ferlui32 <luizferri@gmail.com>
2023-03-20 14:51:23 +0000

    names

---
+++ index.js
@@ -0,0 +1,10 @@
+const myName = function() {
+    names = ["Gosia", "Tomás", "José", "Nuno", "Sjoerd"]
+    for (let i=0; i<names.length; i++){
+        console.log(names[i])
+    }
+    
+};
+
+
+myName();
